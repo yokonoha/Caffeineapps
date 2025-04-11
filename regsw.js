1d5287e3c3adba377cb0f06acd11e467ded5a11e
横茶横葉/Y.Yokoha <askjp.team@gmail.com>
2025-04-11 09:43:27 +0900

    regsw.js の更新

--- regsw.js
+++ regsw.js
@@ -1,6 +1,6 @@
 //Service-worker implementation
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', function() {
-  navigator.serviceWorker.register('/Caffeine_Movie/service-worker.js');
+  navigator.serviceWorker.register('/service-worker.js');
   });
 }
